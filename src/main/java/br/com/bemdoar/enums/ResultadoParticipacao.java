ef39704340a934d73303fa0e300e8a108be9f7fa
Felipe20056 <felipeca20056@gmail.com>
2026-09-12 13:56:53 -0300

    fix: recarrega a estrutura do backend

--- src/main/java/br/com/bemdoar/enums/ResultadoParticipacao.java
+++
@@ -1,5 +0,0 @@
-package br.com.bemdoar.enums;
-
-public enum ResultadoParticipacao {
-    PRESENTE, AUSENTE, CANCELADA
-}
--- src/main/java/br/com/bemdoar/enums/SituacaoCandidatura.java
+++
@@ -1,5 +0,0 @@
-package br.com.bemdoar.enums;
-
-public enum SituacaoCandidatura {
-    PENDENTE, APROVADA, RECUSADA, CANCELADA
-}
